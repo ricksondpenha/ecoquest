83679b6734911ef441528e85d091fc1e56b9682a
Rickson Dpenha <ricksondpenha@gmail.com>
2024-01-14 01:09:48 +0100

    adds riverpod dependencies

---
+++ lib/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract class AppInit {
+  static Future<ProviderContainer> init() async {
+    final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+    // Put game into full screen mode on mobile devices.
+    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    // Lock the game to portrait mode on mobile devices.
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    final provider = ProviderContainer(
+      overrides: [],
+      observers: [],
+    );
+
+    return provider;
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
