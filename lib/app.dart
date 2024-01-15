0d6ae34594d7ada9d7ca206d5b78b88c88712e93
Rickson Dpenha <ricksondpenha@gmail.com>
2024-01-15 16:50:30 +0100

    adds native splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'config/app_theme.dart';
 import 'core/services/app_lifecycle.dart';
 import 'core/services/app_navigation.dart';
 import 'core/services/local_storage.dart';
+import 'core/utils/provider_logger.dart';
 
 /// This class is responsible for initializing the app.
 abstract class AppInit {
   // Initializes the app.
   static Future<ProviderContainer> init() async {
     final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+    // Show splash screen.
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
     // Put game into full screen mode on mobile devices.
     await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
@@ -20,16 +25,16 @@ abstract class AppInit {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-
+    
     final provider = ProviderContainer(
       overrides: [],
-      observers: [],
+      observers: [ProviderLogger()],
     );
 
     try {
     // Register app lifecycle listener.
     provider.read(appLifecycleProvider);
-    
+
       // Initialize local storage.
       await provider.read(localStorageProvider).init();
 
@@ -37,6 +42,9 @@ abstract class AppInit {
     } catch (e, s) {
       debugPrint('${e.toString()}, ${s.toString()}');
     }
+
+    // Remove splash screen.
+    FlutterNativeSplash.remove();
 
     return provider;
   }
@@ -50,7 +58,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer(builder: (context, ref, child) {
       final appTheme = ref.watch(appThemeProvider);
-    return MaterialApp.router(
+      return MaterialApp.router(
       routerConfig: AppNavigation.router,
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
---
+++ lib/core/utils/provider_logger.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProviderLogger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    debugPrint('''
+{
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "$newValue"
+}''');
+  }
+}
